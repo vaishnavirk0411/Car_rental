729f4977cd7c3c11edc9ae856220a38dd68c1db1
vaishnavirk0411 <kulkarnivaishnavi0411@gmail.com>
2020-11-05 12:41:17 +0530

    change

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//Aniket_Boss
 Route::get('/', function () {
     return view('welcome');
 });
